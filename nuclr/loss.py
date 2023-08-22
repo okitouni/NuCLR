5f38a64b98e9ba33091a1c4973917aa7f4e2436a
kitouni <28654657+okitouni@users.noreply.github.com>
2023-08-22 13:04:25 -0400

    [bug] misc typos

--- nuclr/loss.py
+++ nuclr/loss.py
@@ -1,6 +1,5 @@
 import torch
-from .utils import TensorDict, Fields
-
+from .utils import TensorDict
 
 mse = torch.nn.functional.mse_loss
 cce = torch.nn.functional.cross_entropy
@@ -18,30 +17,35 @@ def loss_by_task(preds: TensorDict, targets: TensorDict):
     """
     losses = TensorDict(fields=preds.fields)
     for field in preds.fields.all_fields:
-        mask = ~targets[field].isna()
+        mask = ~targets[field].isnan().view(-1)
         pred = preds[field][mask]
         target = targets[field][mask]
-        if field in preds.fields.numerical:
+        if field in preds.fields["numerical"]:
             losses[field] = mse(pred, target)
-        elif field in preds.fields.categorical:
+        else:
             losses[field] = cce(pred, target)
     return losses
 
 def metric_by_task(preds: TensorDict, targets: TensorDict, transforms: dict):    
     metrics = TensorDict(fields=preds.fields)
     for field in preds.fields.all_fields:
-        mask = ~targets[field].isna()
-        pred = preds[field][mask]
-        target = targets[field][mask]
-        if field in preds.fields.numerical:
-            pred = transforms[field].inverse_transform(pred)
-            target = transforms[field].inverse_transform(target)
+        mask = ~targets[field].isnan()
+        pred = preds[field][mask].view(-1, 1)
+        target = targets[field][mask].view(-1, 1)
+        if field in preds.fields["numerical"]:
+            pred = transforms[field].inverse_transform(pred.cpu().detach().numpy())
+            target = transforms[field].inverse_transform(target.cpu().detach().numpy())
             eval_fn = get_eval_fn_for(field)
-            pred = eval_fn(pred, targets["z"], targets["n"])
-            target = eval_fn(target, targets["z"], targets["n"])
-            metrics[field] = mse(pred, target).sqrt()
-        elif field in preds.fields.categorical:
-            metrics[field] = (pred.argmax(dim=-1) == target).float().mean()
+            z = targets["z"][mask].view(-1, 1).cpu().detach().numpy()
+            n = targets["n"][mask].view(-1, 1).cpu().detach().numpy()
+            
+            pred = eval_fn(pred, z, n)
+            target = eval_fn(target, z, n)
+            
+            rmse = ((pred-target)**2).mean()**0.5
+            metrics[field] = torch.tensor([rmse])
+        else:
+            metrics[field] = (pred.argmax(dim=-1, keepdim=True) == target).float().mean()
     return metrics
 
 def get_eval_fn_for(task_name):
@@ -50,4 +54,4 @@ def get_eval_fn_for(task_name):
       return output * (n + z)
     return eval_fn
   else:
-    return lambda x, _: x
+    return lambda x, *_: x
